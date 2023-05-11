bce6d020ea5d2ad0e1a7cde5fa1a4727dbb4fa8b
Wystov <wystov@gmail.com>
2023-05-11 12:47:19 +0300

    fix: win condition

--- minesweeper/src/js/game-end.js
+++ minesweeper/src/js/game-end.js
@@ -1,11 +1,12 @@
 import state from './state';
+import cells from './data';
 
 const checkGameEnd = (value) => {
   const { fieldSize, mines, turns } = state;
   if (value === 'x') {
     console.log('Game over. Try again');
   }
-  if (turns === fieldSize - mines) {
+  if (cells.open.length === fieldSize - mines) {
     console.log(`Hooray! You found all mines in ## seconds and ${turns} moves!`);
   }
 };
